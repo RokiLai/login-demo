ff9af5821e32b0186dd150060cf0d5d2862fd0e4
RokiLai <RokiLai@icloud.com>
2025-07-17 17:42:33 +0800

    1.配置跨域

--- login-api/src/main/java/com/example/logindemo/config/WebConfig.java
+++ login-api/src/main/java/com/example/logindemo/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.logindemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/error"); // 拦截所有请求
 
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 允许所有路径
+                .allowedOriginPatterns("*") // 允许任意源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+
 }
